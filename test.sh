6b7c296e2f9601941c4fa77f7a168f0236791987
Laurent Dumont <laurentfdumont@gmail.com>
2019-06-10 22:24:00 -0400

    More Stuff

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+#test1
+source venv/bin/activate.fish  
+git add .
+git commit -m "More Stuff"
+git push origin master
+mkdocs gh-deploy
+git add .
+git commit -m "More Stuff!"
+git push origin master
